5522728d1dfe1f4d2a691df9c61f33e00710208e
Roman <rusanov.r61@gmail.com>
2026-03-28 22:27:52 +0300

    Add comments.

--- Example1.dart
+++ Example1.dart
@@ -1,20 +1,27 @@
+/*
+    многострочный комментарий
+    Первая программа
+    на языке Dart
+*/
+// определение функции main
 void main()
 {
+  // Тип_данных имя_переменной;
   String name = "Tom";
-  print(name);
+  print(name);          // вывод строки на консоль
 
   var name1 = "Bob";
   print(name1);
 
-  dynamic name2 = "Cat";
+  dynamic name2 = "Cat";    //dynamic позволяет изменять тип переменной.
   print(name2);
   name2 =  45;
   print(name2);
 
-  const name3 = "Sam";
+  const name3 = "Sam";    //const должно быть определено при компиляции
   print(name3);
 
-  final today = DateTime.now();
+  final today = DateTime.now();   // final определяется во время выполнения.
   print(today);
 
 }
