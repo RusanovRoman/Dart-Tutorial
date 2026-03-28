f46ce9c5aa8f5b1d101120f6e4a3d09ba98e72bd
Roman <rusanov.r61@gmail.com>
2026-03-28 22:24:55 +0300

    Chapter 2. Example 1.

---
+++ Example1.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  String name = "Tom";
+  print(name);
+
+  var name1 = "Bob";
+  print(name1);
+
+  dynamic name2 = "Cat";
+  print(name2);
+  name2 =  45;
+  print(name2);
+
+  const name3 = "Sam";
+  print(name3);
+
+  final today = DateTime.now();
+  print(today);
+
+}
