c51300b762e8848d2ac53334cabf8a36481bc10a
Roman <rusanov.r61@gmail.com>
2026-04-11 17:39:49 +0300

    Chapter 8. Directories.

---
+++ Filesystem/Directories.dart
@@ -0,0 +1,70 @@
+/*
+Для работы с файловой системой и вообще системой ввода/ввода в языке Dart предназначен dart:io,
+который определяет соответствующий функционал.
+В частности, для работы с каталогами в этом пакете определен класс Directory
+*/
+
+import 'dart:io';
+
+void main()
+{
+  print(Directory.current);       // Получить путь к текущей папке
+  print(Directory.systemTemp);    // Каталог для временных файлов в текущей системе
+
+  var myDir = Directory("hello");
+  myDir.create();                 // Создание каталога
+  print("Folder has been created");
+
+  var myDirRec = Directory("hello1/temp");    // Если нужно создать каталог с подкаталогом
+  myDirRec.create(recursive: true);           // дополнительно передаем пареметр
+
+  GetInfoDir();
+
+  FileExist();
+
+  RenameDir();
+
+  RemoveDir();
+}
+
+// Получение содержимого
+void GetInfoDir() async
+{
+  var myDir = Directory("C:\\");
+  try{
+    // С помошью метода list() можно получить содержимое папки - вложенные папки и файлы
+    await for (var entity in myDir.list()) {
+      print(entity.path);
+    }
+  }
+  catch(ex)
+  {
+    print(ex);
+  }
+}
+
+// Проверка существования директории
+// Метод exists() возвращает true, если директория существует
+void FileExist() async
+{
+  print(await Directory("C:\"").exists());
+  print(await Directory("somefolder").exists());
+}
+
+// Для переименования директории применяется метод rename()
+void RenameDir() async
+{
+  // создаем директорию "hello"
+  var dir = await Directory("hello").create();
+  // переименовываем ее в "work"
+  var new_dir = await dir.rename("work");
+  print(new_dir.path);  // work
+}
+
+// Для удаления директории применяется метод delete()
+void RemoveDir() async
+{
+  var dir = Directory("work");
+  await dir.delete();
+  print("${dir.path} deleted");
+}
